b00f3bcf4a530e8aa350903aacf376b6821ed8e1
adarsh <adarsh@localhost.localdomain>
2018-11-06 19:00:26 +0530

    first is done

--- chap1/prob1.cc
+++ chap1/prob1.cc
@@ -1,15 +1,35 @@
 #include <iostream>
 #include <vector>
-
-
+#include <string> 
+#include <map>
 
 using namespace std;
 
+bool isUnique(string str)
+{
+	map<char,int> m_occurence;
+
+	for(char ch:str){
+		if(m_occurence[ch] == 0 )
+			m_occurence[ch]++;
+		else
+			return false;
+	}
+	return true;
+}
+
+
 int main()
 {
-	
+	string str="Helo" ;
+	if(isUnique(str))
+		cout<<"The string is unique "<< endl;
+	else
+		cout<<"The string provided does not contain unique character ----" << endl;
 
 
 
 
+
+	return 0;
 }
